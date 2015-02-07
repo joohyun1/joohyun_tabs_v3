d792fd168c60082adbc86575c3828fc6a1666a68
aanunez <adam.a.nunez@gmail.com>
2015-02-07 13:24:56 -0500

    Minor changes removed url encoding on the title and gave a default file name if none is specified.

--- folder.js
+++ folder.js
@@ -1,4 +1,5 @@
-var folder_name = location.search.slice(1)
+var folder_name = location.search.slice(1);
+folder_name = decodeURIComponent(folder_name);
 console.log(folder_name);
 document.getElementById("title").innerHTML = folder_name;
 document.title = folder_name;
