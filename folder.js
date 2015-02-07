dbb49bfe154911ed9b6312432856a614878e664b
Sam <walczaksam@gmail.com>
2015-02-07 15:40:25 -0500

    Made fonts bigger
    that's it

--- folder.js
+++ folder.js
@@ -1,4 +1,4 @@
 window.onload = function() {var folder_name = location.search.slice(1);
 console.log(folder_name);
-document.getElementById("title").innerHTML = folder_name + "Folder";
+document.getElementById("title").innerHTML = folder_name + " Folder";
 document.title = folder_name; }
